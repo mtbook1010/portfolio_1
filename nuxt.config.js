ffbba1225fc9954cce52469362af00721d6823f4
mtbook1010 <mt_book@outlook.jp>
2020-05-04 23:17:25 +0900

    32

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,15 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:url', property: 'og:url', content: 'https://shomayamamoto.netlify.app/' },
+      { hid: 'og:title', property: 'og:title', content: this.title },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:description', property: 'og:description', content: '' },
+      { hid: 'og:image', property: 'og:image', content: '~/assets/img/ogp-img' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Shoma Yamamoto Portfolio' },
+      { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' }
     ],
     script: [
       { src: 'hm.js' }
