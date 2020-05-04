1d992681f30e84267f0794d8bdbe8eb22048f9c4
mtbook1010 <mt_book@outlook.jp>
2020-05-04 23:18:10 +0900

    33

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { hid: 'description', name: 'description', content: '' },
       { hid: 'og:url', property: 'og:url', content: 'https://shomayamamoto.netlify.app/' },
       { hid: 'og:title', property: 'og:title', content: this.title },
